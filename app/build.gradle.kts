e967655107c63192ad8151cea96b92ab5f366cd2
Ayra Hikari <AyraHikari@gmail.com>
2025-10-08 22:58:06 +0700

    Version 0.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "me.ayra.ha.healthconnect"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "0.3"
+        versionCode = 4
+        versionName = "0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
